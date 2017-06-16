77fb7aec181998eecb92a3ae4ea62a973e471acc
agdhruv <dhruv_agarwal@live.com>
2017-06-16 15:42:29 +0530

    Remove env.

--- env/lib/python2.7/site-packages/pyshorteners/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-__version__ = '0.6.0'
-__author__ = 'Ellison Leão'
-__license__ = 'MIT'
-
-# flake8: noqa
-from .shorteners import Shortener, Shorteners
