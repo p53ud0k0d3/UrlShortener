94372c20d37202d70c1ccf5219af6d30969f2aef
khvilaboa <khvilaboa@esei.uvigo.es>
2017-06-26 20:03:30 +0200

    Bit.ly support

--- Shortener/views.py
+++ Shortener/views.py
@@ -3,12 +3,16 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
-
 from pyshorteners import Shortener
 from .forms import Urlform
 
+BITLY_TOKEN = "19c73c3f96d4b2a64d0337ef7380cf0de313e8f7"
+
 def worker(url, host):
-    shortener = Shortener(host, timeout=10)
+    if host == "Bitly":
+        shortener = Shortener("Bitly", timeout=10, bitly_token=BITLY_TOKEN)
+    else:
+        shortener = Shortener(host, timeout=10)
     short_url = shortener.short(url)
     return short_url
 
