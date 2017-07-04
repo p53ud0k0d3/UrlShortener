34d31a1bbe278cfcbe4fefe4a878e16fae9320dc
Jay Welborn <jesse.welborn@gmail.com>
2017-07-04 13:02:26 +0200

    updated .gitignore, moved SECRET_KEY and database out of version control

--- UrlShortener/settings.py
+++ UrlShortener/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os
 
-from private_settings import SECRET_KEY
+from private_settings import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
