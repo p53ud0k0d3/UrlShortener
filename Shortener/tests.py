cb70ab98f06422c809626034eab8e64c3d585bcc
Patrick Nicholls <padawagon@gmail.com>
2017-07-09 14:55:05 +1200

    Setup pytest infrastructure
    Installed dependencies to get pytest to work with django. pytest-pythonpath helps find tests and manage.py for pytest.
    setup.py makes installing the application easier.

--- Shortener/tests.py
+++ Shortener/tests.py
@@ -3,4 +3,14 @@ from __future__ import unicode_literals
 
 from django.test import TestCase
 
-# Create your tests here.
+from Shortener.views import worker
+
+# Model tests
+# Form tests
+# View tests
+
+def test_worker_shortens_url_with_google():
+    url = "http://7bna.net/wallpapers/cat-pictures.html"
+    host = "Google"
+
+    assert len(worker(url, host)) < len(url)
