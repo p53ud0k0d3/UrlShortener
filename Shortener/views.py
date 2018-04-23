a9a9431ceac366767f51f7a62e85dea5f9cdc5ce
Brent Sterckx <brent@live.be>
2018-04-23 17:07:37 +0200

    Add DA.GD functionality

--- Shortener/views.py
+++ Shortener/views.py
@@ -28,6 +28,8 @@ def worker(url, host):
         shortener = Shortener(engine=Rebrandly, timeout=10, api_key=REBRANDLY_TOKEN)
     elif host == "Madwire":
         shortener = Shortener(engine=Madwire, timeout=10)
+    elif host == "Dagd":
+        shortener = Shortener("Dagd", timeout=10)
     else:
         shortener = Shortener(host, timeout=10)
     short_url = shortener.short(url)
