7b8bf93b6b20dda43bc65b14e309e43bea566ca5
Roberto Ruccia <roberto.ruccia@gmail.com>
2018-11-07 00:15:29 +0100

    Update forms

--- Shortener/forms.py
+++ Shortener/forms.py
@@ -2,14 +2,14 @@ from django import forms
 
 
 HOSTS = (
-        ('Tinyurl', 'Tinyurl'),
-#        ('Isgd', 'Is.gd'),      # not recognized by pyshorteners, fix it
-	('Bitly', 'Bit.ly'),
-        ('Rebrandly', 'Rebrand.ly'),
-        ('Madwire', 'm360.us'),
-        ('Osdb', 'Osdb.link')
-#        ('Soogd', 'Soo.gd')
-    )
+    ('Tinyurl', 'Tinyurl'),
+    ('Isgd', 'Is.gd'),
+    ('Bitly', 'Bit.ly'),
+    ('Rebrandly', 'Rebrand.ly'),
+    ('Madwire', 'm360.us'),
+    ('Osdb', 'Osdb.link'),
+#    ('Soogd', 'Soo.gd')    # not working in pyshorteners
+)
 
 class Urlform(forms.Form):
     url = forms.CharField(initial='http://',required=True)
