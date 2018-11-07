972c7b69e191cae3352510981b3340a42d0094bb
Roberto Ruccia <roberto.ruccia@gmail.com>
2018-11-07 11:57:17 +0100

    update tests

--- Shortener/tests.py
+++ Shortener/tests.py
@@ -1,33 +1,21 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from urllib.parse import urlparse
-
 from django.core.validators import URLValidator
-
 from Shortener.views import worker
+from .forms import HOSTS
 
 # Model tests
 # Form tests
 # View tests
 
-def test_worker_shortens_url_with_bitly():
+def test_worker_shortens_url():
     url = "http://7bna.net/wallpapers/cat-pictures.html"
-    host = "Bitly"
 
-    shortened_url = worker(url, host)
-
-    assert url_validator(shortened_url)
-    assert len(shortened_url) < len(url)
-
-def test_worker_shortens_url_with_madwire():
-    url = "http://7bna.net/wallpapers/cat-pictures.html"
-    host = "Madwire"
-
-    shortened_url = worker(url, host)
-
-    assert url_validator(shortened_url)
-    assert len(shortened_url) < len(url)
+    for host, _ in HOSTS:
+        shortened_url = worker(url, host)
+        assert url_validator(shortened_url)
+        assert len(shortened_url) < len(url)
 
 
 def url_validator(url):
