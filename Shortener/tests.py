be689c572bc64bd0078f4ae813bd35e459875f30
Roberto Ruccia <roberto.ruccia@gmail.com>
2018-11-07 10:26:55 +0100

    remove google test

--- Shortener/tests.py
+++ Shortener/tests.py
@@ -11,15 +11,6 @@ from Shortener.views import worker
 # Form tests
 # View tests
 
-def test_worker_shortens_url_with_google():
-    url = "http://7bna.net/wallpapers/cat-pictures.html"
-    host = "Google"
-
-    shortened_url = worker(url, host)
-
-    assert url_validator(shortened_url)
-    assert len(shortened_url) < len(url)
-
 def test_worker_shortens_url_with_bitly():
     url = "http://7bna.net/wallpapers/cat-pictures.html"
     host = "Bitly"
