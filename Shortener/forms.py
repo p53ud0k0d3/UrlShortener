04379be01fa83c8c58b3f33df33189ad64afd58d
David Mason <djmot@users.noreply.github.com>
2019-01-10 17:11:00 -0500

    add clck.ru, qps.ru

--- Shortener/forms.py
+++ Shortener/forms.py
@@ -4,11 +4,13 @@ from django import forms
 HOSTS = (
         ('Tinyurl', 'Tinyurl'),
         ('Isgd', 'Is.gd'),
-		    ('Bitly', 'Bit.ly'),
+        ('Bitly', 'Bit.ly'),
         ('Google', 'Google URL Shortener'),
         ('Rebrandly', 'Rebrand.ly'),
         ('Madwire', 'm360.us'),
-        ('Osdb', 'Osdb.link	')
+        ('Osdb', 'Osdb.link	'),
+        ('Clckru', 'Clck.ru'),
+        ('Qpsru', 'Qps.ru')
         )
 
 class Urlform(forms.Form):
