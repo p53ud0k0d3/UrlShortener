33005e6b7a70cb31d60853c8634da2c2ebdc0580
danny crasto <danwald79@gmail.com>
2019-01-16 21:19:06 +0400

    Parameterized tests

--- Shortener/tests.py
+++ Shortener/tests.py
@@ -5,47 +5,24 @@ from urllib.parse import urlparse
 
 from django.core.validators import URLValidator
 
+import pytest
 from Shortener.views import worker
 
-# Model tests
-# Form tests
-# View tests
+URL = 'http://7bna.net/wallpapers/cat-pictures.htm'
 
-def test_worker_shortens_url_with_google():
-    url = "http://7bna.net/wallpapers/cat-pictures.html"
-    host = "Google"
 
+@pytest.mark.parametrize('host,url', [
+    ('Google', URL),
+    ('Bitly', URL),
+    ('Madwire', URL),
+    ('Rebrandly', URL),
+    ('isgd', URL),
+])
+def test_worker_shortens_url_with_(host, url):
     shortened_url = worker(url, host)
-
     assert url_validator(shortened_url)
     assert len(shortened_url) < len(url)
 
-def test_worker_shortens_url_with_bitly():
-    url = "http://7bna.net/wallpapers/cat-pictures.html"
-    host = "Bitly"
-
-    shortened_url = worker(url, host)
-
-    assert url_validator(shortened_url)
-    assert len(shortened_url) < len(url)
-
-def test_worker_shortens_url_with_madwire():
-    url = "http://7bna.net/wallpapers/cat-pictures.html"
-    host = "Madwire"
-
-    shortened_url = worker(url, host)
-
-    assert url_validator(shortened_url)
-    assert len(shortened_url) < len(url)
-
-def test_worker_shortens_url_with_isgd():
-    url = "http://7bna.net/wallpapers/cat-pictures.html"
-    host = "isgd"
-
-    shortened_url = worker(url, host)
-
-    assert url_validator(shortened_url)
-    assert len(shortened_url) < len(url)
 
 def url_validator(url):
     try:
