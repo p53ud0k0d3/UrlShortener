847c7ac3948fcaa1b0cf5ee789a16acc3257840c
Harrison Mitchell <Harrison-Mitchell@users.noreply.github.com>
2019-10-02 10:32:24 +1000

    Remove goo.gl

--- Shortener/forms.py
+++ Shortener/forms.py
@@ -4,12 +4,11 @@ from django import forms
 HOSTS = (
         ('Tinyurl', 'Tinyurl'),
         ('Isgd', 'Is.gd'),
-		    ('Bitly', 'Bit.ly'),
-        ('Google', 'Google URL Shortener'),
+        ('Bitly', 'Bit.ly'),
         ('Rebrandly', 'Rebrand.ly'),
         ('Madwire', 'm360.us'),
         ('Osdb', 'Osdb.link	')
-        )
+)
 
 class Urlform(forms.Form):
     url = forms.CharField(initial='http://',required=True)
